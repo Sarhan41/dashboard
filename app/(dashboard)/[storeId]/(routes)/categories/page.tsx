59d7d7b21eb4e8015c00fa9b388e53976a97851a
Sarhan41 <sp.webdev2024@gmail.com>
2024-01-23 12:36:39 +0530

    Refactor category components and update API calls

--- app/(dashboard)/[storeId]/(routes)/categories/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -1,29 +1,33 @@
 import { format } from "date-fns";
 
-import { BillboardClient } from "./components/client";
+import { CategoryClient } from "./components/client";
 import prismadb from "@/lib/prismadb";
-import { BillboardColumn } from "./components/columns";
+import { CategoryColumn } from "./components/columns";
 
 const CategoriesPage = async ({ params }: { params: { storeId: string } }) => {
-  const categories = await prismadb.billboard.findMany({
+  const categories = await prismadb.category.findMany({
     where: {
       storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
     },
     orderBy: {
       createdAt: "desc",
     },
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
     id: item.id,
-    label: item.label,
+    name: item.name,
+    billboardLabel: item.billboard.label,
     createdAt: format(item.createdAt, "MMM do, yyyy"),
   }));
 
   return (
     <div className="  flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedBillboards} />
+        <CategoryClient data={formattedCategories} />
       </div>
       <div></div>
     </div>
